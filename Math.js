fcdd3ec82cfb0d28797b4881559f220dfcf8117e
Anunay-Anand <anunayanand@gmail.com>
2021-10-01 23:15:56 +0530

    completed stack question

--- Math.js
+++ Math.js
@@ -50,40 +50,70 @@ var isPowerOfThree = function (n) {
   return true;
 };
 
-// 4) Valid Paranthesis 
+// 4) Valid Paranthesis
 
 function isValid(s) {
-        // Index of string         
-        let i = 0;
-        // Bracket object          
-        const brackets = {
-            "{": "}",
-            "[": "]",
-            "(": ")"
-        };
-        // Stack for verifying right brackets         
-        const stack = [];
-        while(i < s.length) {
-            if(s[i] === "{" || s[i] === "[" || s[i] === "(") {
-                stack.push(s[i]);
-            } else {
-                if(brackets[stack.pop()] !== s[i]) {
-                    return false;
-                }
-            }
-            i++;
-        }
-        return stack.length === 0;
-    };
+  // Index of string
+  let i = 0;
+  // Bracket object
+  const brackets = {
+    "{": "}",
+    "[": "]",
+    "(": ")",
+  };
+  // Stack for verifying right brackets
+  const stack = [];
+  while (i < s.length) {
+    if (s[i] === "{" || s[i] === "[" || s[i] === "(") {
+      stack.push(s[i]);
+    } else {
+      if (brackets[stack.pop()] !== s[i]) {
+        return false;
+      }
+    }
+    i++;
+  }
+  return stack.length === 0;
+}
 
 // 5) Missing Number
 
-var missingNumber = function(nums) {
+var missingNumber = function (nums) {
   let result = 0;
-  
+
   for (let i = 0; i < nums.length; i++) {
     result = result + i + 1 - nums[i];
   }
-  
+
   return result;
+};
+
+// 6) Stack design
+
+MinStack.prototype.push = function (x) {
+  this.elements.push({
+    value: x,
+    min: this.elements.length === 0 ? x : Math.min(x, this.getMin()),
+  });
+};
+/**
+
+ @return {void}
+ */
+MinStack.prototype.pop = function () {
+  this.elements.pop();
+};
+/**
+
+ @return {number}
+ */
+MinStack.prototype.top = function () {
+  return this.elements[this.elements.length - 1].value;
+};
+/**
+
+ @return {number}
+ */
+MinStack.prototype.getMin = function () {
+  return this.elements[this.elements.length - 1].min;
 };
