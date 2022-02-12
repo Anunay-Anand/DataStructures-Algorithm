c56394dbe6806556ac626982056e2ab3a5b99dc8
Anunay-Anand <anunayanand@gmail.com>
2022-02-12 21:35:28 +0530

    circular queue

--- queue.js
+++ queue.js
@@ -58,9 +58,88 @@ class Queue {
 }
 
 const myQueue = new Queue();
-myQueue.enqueue("jerry");
-myQueue.enqueue("tom");
-myQueue.enqueue("spike");
-myQueue.dequeue();
-myQueue.dequeue();
-myQueue.dequeue();
+myQueue.enqueue(1);
+myQueue.enqueue(2);
+myQueue.enqueue(3);
+// myQueue.dequeue();
+// myQueue.dequeue();
+// myQueue.dequeue();
+
+function queueReversal(q) {
+  //your code here
+  let len = q.length;
+  // Check if the Queue is empty
+  if (len === 0) {
+    return null;
+  }
+  let stack = [];
+  while (len > 0) {
+    stack.push(q.dequeue());
+    len--;
+  }
+
+  while (stack.length > 0) {
+    q.enqueue(stack.pop());
+  }
+
+  return q;
+}
+
+console.log(queueReversal(myQueue));
+// 2) Create a Queue using Stack
+
+// var MyQueue = function() {
+//     this.stack1 = [];
+//     this.stack2 = [];
+// };
+
+// /**
+//  * @param {number} x
+//  * @return {void}
+//  */
+// MyQueue.prototype.push = function(x) {
+//     this.stack1.push(x);
+//     return null;
+// };
+
+// /**
+//  * @return {number}
+//  */
+// MyQueue.prototype.pop = function() {
+//     if(this.stack2.length > 0) {
+//         return this.stack2.pop();
+//     } else {
+//     while(this.stack1.length > 0) {
+//         this.stack2.push(this.stack1.pop());
+//     }
+//     if(this.stack2.length === 0) {
+//         return null;
+//     }
+//     return this.stack2.pop();
+//     }
+// };
+
+// /**
+//  * @return {number}
+//  */
+// MyQueue.prototype.peek = function() {
+//     if(this.stack2.length === 0) {
+//        while(this.stack1.length > 0) {
+//        this.stack2.push(this.stack1.pop());
+//     }
+//     }
+//     if(this.stack2.length === 0) {
+//         return -1;
+//     }
+//     return this.stack2[this.stack2.length - 1];
+// };
+
+// /**
+//  * @return {boolean}
+//  */
+// MyQueue.prototype.empty = function() {
+//     if(this.stack1.length === 0 && this.stack2.length === 0) {
+//         return true;
+//     }
+//     return false;
+// };
