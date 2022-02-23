da77fa1a753479e93b2d37e43d827b812d51b7af
Anunay-Anand <anunayanand@gmail.com>
2022-02-23 14:28:40 +0530

    min stack corrected solution

--- queue.js
+++ queue.js
@@ -92,15 +92,10 @@ myQueue.enqueue(3);
 // };
 
 // MyQueue.prototype.peek = function() {
-//     if(this.stack2.length === 0) {
-//        while(this.stack1.length > 0) {
-//        this.stack2.push(this.stack1.pop());
-//     }
-//     }
-//     if(this.stack2.length === 0) {
-//         return -1;
-//     }
-//     return this.stack2[this.stack2.length - 1];
+//   if(this.stack2.length === 0 && this.stack1.length !== 0) {
+//     return this.stack1[0];
+// }
+// return this.stack2[this.stack2.length - 1];
 // };
 
 // MyQueue.prototype.empty = function() {
